18f3182192dc1389f417edf1e83bbb44da085fd4
SatanshuMishra <satanshumishra@outlook.com>
2023-05-21 21:00:59 -0700

    Completed Section 3

---
+++ complete-react-guide/src/components/ExpenseComponents/ExpenseDate.js
@@ -0,0 +1,18 @@
+import Card from "../UI/Card";
+import "./ExpenseDate.css";
+
+function ExpenseDate(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
+  return (
+    <Card className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__day">{day}</div>
+      <div className="expense-date__year">{year}</div>
+    </Card>
+  );
+}
+
+export default ExpenseDate;
